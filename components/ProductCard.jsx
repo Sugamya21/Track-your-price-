467480bbb9249d66410e49b9de238e7c540d20e8
Sugamya21 <sugamyapatkari@gmail.com>
2026-03-01 23:24:16 +0530

    Add email alerts

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -11,6 +11,7 @@ import { Button } from "./ui/button";
 import { Trash2, ChevronUp, ChevronDown } from "lucide-react";
 import Link from "next/link";
 import { getPriceHistory } from "@/app/actions";
+import PriceChart from "./PriceChart";
 
 const ProductCard = ({ product }) => {
     const [showChart, setShowChart] = useState(false);
